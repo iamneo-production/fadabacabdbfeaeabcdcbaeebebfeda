d83fe1686a8b9b7115295f67ce14e62a94b685b8
Iamneo <techadmin@iamneo.ai>
2023-09-06 12:32:36 +0000

    Backup Commit

--- angularapp/src/app/app-routing.module.ts
+++ angularapp/src/app/app-routing.module.ts
@@ -8,18 +8,4 @@ const routes: Routes = [];
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
-export class AppRoutingModule { }
-
-//app.component.html
-
-<header>
-  <h3 id="headerContainer">Calory Tracker</h3>
-</header>
-<div class="container">
-  <div class="food-container">
-      <app-food-list></app-food-list>
-  </div>
-</div>
-<footer>
-  Happy Coding ProGrad <span><img src="../assets/prograd-heart.svg" alt=""></span>!
-</footer>
+export class AppRoutingModule { 
